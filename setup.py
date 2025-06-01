a7855a1dce52154976157ca8a5c86fac5305415c
Navid <mahdian.navid@gmail.com>
2025-06-01 14:45:11 -0700

    another typo fixed

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ data_files.append(('share/' + package_name + '/protos', [
     'protos/LegoTallInterval.proto',
     'protos/LegoTallWall.proto',
     'protos/Pi-puck.proto',
-    'E-puck.proto',
+    'protos/E-puck.proto',
 ]))
 data_files.append(('share/' + package_name + '/resource', [
     'resource/all.rviz',
